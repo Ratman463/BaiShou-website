a0988e8a83d1059e835d718db99758d9b511fad2
Alice <alice_kei@163.com>
2026-07-01 01:41:42 +0800

    重构官网首页与主题系统
    首屏改为左右布局，新增浅色深色主题、团队区块与应用图标展示。

---
+++ src/data/site.ts
@@ -0,0 +1,17 @@
+export const REPO_URL = 'https://github.com/foxletters-hq/BaiShou-Next';
+
+export const FOUNDER = {
+  name: 'Anson',
+  role: '创始人',
+  github: 'https://github.com/Anson-Trio',
+  avatar: 'https://avatars.githubusercontent.com/u/107763915?v=4',
+};
+
+export const CONTRIBUTORS = [
+  {
+    name: 'Ratman',
+    role: '贡献者',
+    github: 'https://github.com/Ratman463',
+    avatar: 'https://avatars.githubusercontent.com/u/187963490?v=4',
+  },
+];
